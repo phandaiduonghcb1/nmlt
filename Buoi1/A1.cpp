7cfdc55629ce0df77635a261c4517e760cf90554
phandaiduonghcb1 <59389157+phandaiduonghcb1@users.noreply.github.com>
2020-01-02 19:46:44 +0700

    Update A1.cpp

--- Buoi1/A1.cpp
+++ Buoi1/A1.cpp
@@ -6,6 +6,6 @@ using namespace std;
 int main()
 {
     cout << "Chao mung ban da den voi UIT Truong DH Cong nghe Thong tin!" << endl;
-    cout <<"Welcome !!!";
+    cout <<"Welcomeeeeeeeeeee !!!";
     return 0;
 }
